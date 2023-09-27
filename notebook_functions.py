5a86922933684d8c3e6049ec679efd6586895981
CarlaOnate <carlaonategardella@gmail.com>
2023-09-27 17:55:59 -0600

    added trend function to predict the angles better. Missing second ambiguity

--- notebook_functions.py
+++ notebook_functions.py
@@ -104,7 +104,8 @@ def table_for_views_w_spheroid(images, descriptions, size=(25, 25)):
         row = i // num_cols
         col = i % num_cols
 
-        ax = axes[row, col]
+        # ax = axes[row, col]
+        ax = axes[row, col] if num_rows > 1 else axes[col]
 
         ax.imshow(img)
         ax.axis('off')  # Hide axis
