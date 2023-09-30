11e2d524a2344316a9463481a30ac1f99a5ee77e
CarlaOnate <carlaonategardella@gmail.com>
2023-09-29 21:49:59 -0600

    fixed choose smooth angle, added code to angle estiimation py to estimate angles for all tomatoes and mandarins

--- Point_313/angle_estimation.py
+++ Point_313/angle_estimation.py
@@ -12,74 +12,93 @@ def join_images(images):
     joined_imgs = cv2.hconcat(resized_imgs)
     return joined_imgs
 
+def prev_post_values_b(current_b_index, b_list, steps, direction = 0):
+    enlarged_b_list = [*b_list, *b_list]
+    enlarged_index = current_b_index + len(b_list) - 1 if current_b_index < (len(b_list) / 2) else current_b_index
+    total_steps = 3 + steps
 
-def prev_post_values_b(current_b_index, b_list):
-    b_axes = []
-    # Calculate prev_b and post_b based on index
-    if current_b_index == 0:  # Grab last 3 items
-        b_axes = b_list[-3:]
-    elif  current_b_index == 1:
-        b_axes = [b_list[-1], *b_list[:current_b_index + 1]] # Grab last item and the first two
-    elif current_b_index == len(b_list) - 1:
-        b_axes = [*b_list[current_b_index - 2:], b_list[0]]  # Grab last two item and first one
+    if direction < 0:
+        b_axes = enlarged_b_list[(enlarged_index - total_steps) : index]
+    elif direction > 0:
+        b_axes = enlarged_b_list[enlarged_index : (enlarged_index + total_steps)]
     else:
-        b_axes = b_list[current_b_index - 2 : current_b_index + 1]  # Grab prev item, current, last item
+        b_axes = enlarged_b_list[enlarged_index - 1 : enlarged_index + 3]
     return b_axes
 
 
 def estimate_b_axe_trend(b_axes):
     mean_seq = mean(b_axes)
     if b_axes[0] < mean_seq < b_axes[-1]:
-        return 1   # "Ascending"
+        return True   # "Ascending"
     elif b_axes[0] > mean_seq > b_axes[-1]:
-        return -1   # "Descending"
+        return False  # "Descending"
     else:
-        return 0   # No clear trend
+        return None   # No clear trend
 
 
-def angle_estimation(A, B, b_i, b_axes):
+def angle_estimation(index_view):
+    if oblate_angles[index_view] != -1: return oblate_angles[index_view]   # Return if already calculated in another cycle
+
+    #Spheroid values
+    A, B = oblate_spheroid
+    b_curr = axe_b_all_views[index_view]
     fruit_rot = 'downwards'
-    cos_theta = math.sqrt((pow(b_i, 2) - pow(B, 2))/(pow(A, 2) - pow(B, 2)))
-    theta1 = np.arccos(cos_theta)
-    theta1 = np.degrees(theta1)
-    # theta2 = 360 - theta1
-    theta2 = -np.arccos(cos_theta)
-    theta2 = np.degrees(theta2)
-    ascending_sequence = None
-    b_axes_trend = estimate_b_axe_trend(b_axes)
+    curr_cos_theta = math.sqrt((pow(b_curr, 2) - pow(B, 2))/(pow(A, 2) - pow(B, 2)))
 
-    if b_axes_trend > 0:
-        ascending_sequence = True
-    elif b_axes_trend < 0:
-        ascending_sequence = False
+    curr_theta_pos = np.degrees(np.arccos(curr_cos_theta))
+    curr_theta_neg = np.degrees(-np.arccos(curr_cos_theta))
+    current_view_angles = (curr_theta_pos, curr_theta_neg)
 
-    print("\n\t b_axes:  ", b_axes)
+    rotation_per_view = 360 / len(axe_b_all_views) + 3
+    correct_theta = lambda ascending_sequence: current_view_angles[0] if ascending_sequence and (fruit_rot == 'downwards') or (not ascending_sequence) and (fruit_rot == 'upwards') else current_view_angles[1]
 
-    # if (b_axes[1] > b_axes[0]) and (fruit_rot == 'downwards') or (b_axes[0] > b_axes[1]) and (fruit_rot == 'upwards'):
-    if ascending_sequence is None:
-        print("NO CLEAR TREND, 2nd ambiguity??")
-        theta = 999
-    elif ascending_sequence and (fruit_rot == 'downwards') or (not ascending_sequence) and (fruit_rot == 'upwards'):
-        theta = theta1
+    # Second ambiguity
+    if 0 <= abs(current_view_angles[0]) <= rotation_per_view or abs(90 - rotation_per_view) <= abs(current_view_angles[0]) >= 0:   # Value calculated is in range or a local extrema
+        if index_view < len(image_files) - 1:
+            temp_correct_theta_value = correct_theta(get_trend_for_view(index_view, 0))
+            oblate_angles[index_view] = temp_correct_theta_value
+            next_view_angle = angle_estimation(index_view + 1)
+            if next_view_angle in [0.0, 90.0, -90.0] or oblate_angles[index_view - 1] in [0.0, 90.0, -90.0]:
+                theta = correct_theta(get_trend_for_view(index_view, 0))
+            else:
+                theta = choose_smooth_angle(oblate_angles[index_view - 1])
+        else:
+            theta = correct_theta(get_trend_for_view(index_view, 0))
     else:
-        theta = theta2
+        theta = correct_theta(get_trend_for_view(index_view, 0))
+
+    oblate_angles[index_view] = theta
     return theta
 
 
 def write_angle_on_img(img_to_draw, angle_text, color = (255, 0, 0)):
     font = cv2.FONT_HERSHEY_SIMPLEX
     org = (50, 50)
-    fontScale = 1
+    fontScale = 1.1
     thickness = 2
 
     cv2.putText(img_to_draw, str(angle_text), org, font, fontScale, color, thickness, cv2.LINE_AA)
     return img_to_draw
 
 
-def calculate_oblate(axes_a, axes_b):
-    a = mean(axes_a)
-    b = min(axes_b)
+def calculate_oblate():
+    a = mean(axe_a_all_views)
+    b = min(axe_b_all_views)
     return [a, b]
+
+def choose_smooth_angle(calc_angle):
+    angles = [0.0, 90.0, -90.0]
+    closest = min(angles, key=lambda x: abs(calc_angle - x))
+    return closest
+
+
+def get_trend_for_view(index_view, direction = 0, step = 0):
+    b_axes = prev_post_values_b(index_view, axe_b_all_views, step, direction)  # Try first with direction 0, in other recursions does direction 1 - forward
+    trend = estimate_b_axe_trend(b_axes)
+    if trend is None:
+        return get_trend_for_view(index_view, 1, step + 1)
+    else:
+        return trend
 
 
 fruit_folders = ["../data/tomatoes/", "../data/mandarins/"]
@@ -96,12 +115,11 @@ for curr_fruit, fruit_path in enumerate(fruit_folders): # Cycle types of fruit f
 
         axe_a_all_views = []
         axe_b_all_views = []
-        oblate_angles = []
+        oblate_angles = [-1 for element in range(len(image_files))]
         oblate_spheroid = []
         oblate_edited_imgs = []
 
         for index, image_name in enumerate(image_files):
-            # Get ellipse from view
             file_path = os.path.join(fruit_path, view_folder, image_name)
             img = cv2.imread(file_path)
             mask = pt311utils.create_mask_from_img(img)
@@ -111,33 +129,22 @@ for curr_fruit, fruit_path in enumerate(fruit_folders): # Cycle types of fruit f
             axe_b_all_views.append(minor_axis_length / 2)
             axe_a_all_views.append(major_axis_length / 2)
 
+        # Calculate oblate from views
+        A = mean(axe_a_all_views)
+        B = min(axe_b_all_views)
+        oblate_spheroid = [A, B]
+
         for index, image_name in enumerate(image_files):
+            # Read current image
             file_path = os.path.join(fruit_path, view_folder, image_name)
             img = cv2.imread(file_path)
-
-            # Calculate oblate
-            oblate_spheroid = calculate_oblate(axe_a_all_views, axe_b_all_views)
-
-            # Calculate angle for view
-            oblate_A = oblate_spheroid[0]
-            oblate_B = oblate_spheroid[1]
-
-            # Obtain prev b and post b
-            b_values = prev_post_values_b(index, axe_b_all_views)
+            img_axes = cv2.imread(file_path)
 
             # Estimate angle
-            oblate_angle = angle_estimation(oblate_A, oblate_B, axe_b_all_views[index], b_values)
-
-            # #Check 2nd ambiguity
-            # if index > 0:
-            #     oblate_angle = choose_smooth_angle(oblate_angles[index - 1], oblate_angle)
-            #     print("OBLATE ANGLE AFTER CHECK ", oblate_angle, "\n")
+            oblate_angle = angle_estimation(index)
 
             write_angle_on_img(img, oblate_angle, (0, 255, 0))  # (image, angle to write, color of the text)
             oblate_edited_imgs.append(img)
-
-            # Store angle on list
-            oblate_angles.append(oblate_angle)
 
         # Join img and store with name
         joined_oblate_images = join_images(oblate_edited_imgs)
